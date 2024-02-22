0011a90e732beb9598ae5720e082b84f9be6f534
Service Account <service@example.com>
2024-02-22 12:41:07 +0000

    IOS-5973 maximumFractionDigits to 8

--- Tangem/Common/Extensions/Decimal+.swift
+++ Tangem/Common/Extensions/Decimal+.swift
@@ -9,21 +9,6 @@
 import Foundation
 
 extension Decimal {
-    func currencyFormatted(code: String, maximumFractionDigits: Int = 18) -> String {
-        let formatter = NumberFormatter()
-        formatter.locale = Locale.current
-        formatter.numberStyle = .currency
-        formatter.usesGroupingSeparator = true
-        formatter.currencyCode = code
-        formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = maximumFractionDigits
-        if code == "RUB" {
-            formatter.currencySymbol = "₽"
-        }
-        // formatter.roundingMode = .down
-        return formatter.string(from: self as NSDecimalNumber) ?? "\(self) \(code)"
-    }
-
     var stringValue: String {
         (self as NSDecimalNumber).stringValue
     }
--- Tangem/Modules/LegacySend/LegacySendViewModel.swift
+++ Tangem/Modules/LegacySend/LegacySendViewModel.swift
@@ -997,3 +997,20 @@ extension LegacySendViewModel {
         }
     }
 }
+
+private extension Decimal {
+    func currencyFormatted(code: String, maximumFractionDigits: Int = 18) -> String {
+        let formatter = NumberFormatter()
+        formatter.locale = Locale.current
+        formatter.numberStyle = .currency
+        formatter.usesGroupingSeparator = true
+        formatter.currencyCode = code
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = maximumFractionDigits
+        if code == "RUB" {
+            formatter.currencySymbol = "₽"
+        }
+        // formatter.roundingMode = .down
+        return formatter.string(from: self as NSDecimalNumber) ?? "\(self) \(code)"
+    }
+}
