167f26ebb4f708b9100d65762b44854fa5ed25be
Service Account <service@example.com>
2024-06-13 19:04:09 +0100

    IOS-6892 Update converter methods parameters

--- Tangem/Modules/Send/Fee/SendFeeViewModel.swift
+++ Tangem/Modules/Send/Fee/SendFeeViewModel.swift
@@ -154,7 +154,7 @@ class SendFeeViewModel: ObservableObject {
                 guard
                     let customFee,
                     let feeCurrencyId = self.walletInfo.feeCurrencyId,
-                    let fiatFee = self.balanceConverter.convertToFiat(value: customFee.amount.value, from: feeCurrencyId)
+                    let fiatFee = self.balanceConverter.convertToFiat(customFee.amount.value, currencyId: feeCurrencyId)
                 else {
                     return nil
                 }
