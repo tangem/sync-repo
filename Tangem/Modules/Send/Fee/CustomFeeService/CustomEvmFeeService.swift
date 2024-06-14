34317e714fcf2fa75244bddaf6938b81b3e5b7a4
Service Account <service@example.com>
2024-06-14 10:17:53 +0100

    IOS-7018 Start fee

--- Tangem/Modules/Send/Fee/CustomFeeService/CustomEvmFeeService.swift
+++ Tangem/Modules/Send/Fee/CustomFeeService/CustomEvmFeeService.swift
@@ -141,7 +141,7 @@ class CustomEvmFeeService {
     }
 }
 
-extension CustomEvmFeeService: CustomFeeService, EditableCustomFeeService {
+extension CustomEvmFeeService: EditableCustomFeeService {
     var customFeeDescription: String? {
         Localization.sendEvmCustomFeeFooter
     }
