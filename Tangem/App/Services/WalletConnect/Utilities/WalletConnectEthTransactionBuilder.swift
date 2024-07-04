bfbc7a3d422fe6c62a31a756bc065804bab1318a
Service Account <service@example.com>
2024-07-04 17:42:03 +0100

    IOS-7246 Add `EthereumNetworkProvider+`

--- Tangem/App/Services/WalletConnect/Utilities/WalletConnectEthTransactionBuilder.swift
+++ Tangem/App/Services/WalletConnect/Utilities/WalletConnectEthTransactionBuilder.swift
@@ -94,23 +94,3 @@ extension CommonWalletConnectEthTransactionBuilder: WalletConnectEthTransactionB
         return transaction
     }
 }
-
-extension EthereumNetworkProvider {
-    func getFee(gasLimit: BigUInt, supportsEIP1559: Bool, gasPrice: BigUInt? = nil) async throws -> EthereumFeeParameters {
-        if supportsEIP1559 {
-            let feeHistory = try await getFeeHistory().async()
-            return EthereumEIP1559FeeParameters(
-                gasLimit: gasLimit,
-                baseFee: feeHistory.marketBaseFee,
-                priorityFee: feeHistory.marketPriorityFee
-            )
-        }
-
-        if let gasPrice {
-            return EthereumLegacyFeeParameters(gasLimit: gasLimit, gasPrice: gasPrice)
-        }
-
-        let gasPrice = try await getGasPrice().async()
-        return EthereumLegacyFeeParameters(gasLimit: gasLimit, gasPrice: gasPrice)
-    }
-}
