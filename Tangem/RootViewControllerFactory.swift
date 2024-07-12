7beb40accf0b0b0321c7151128b87f8652eb4ee8
Service Account <service@example.com>
2024-07-12 19:09:46 +0200

    IOS-6944: Inject all required dependencies into the view hierarchy
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/RootViewControllerFactory.swift
+++ Tangem/RootViewControllerFactory.swift
@@ -12,74 +12,23 @@ import SwiftUI
 
 struct RootViewControllerFactory {
     func makeRootViewController(for rootView: some View) -> UIViewController {
-        let contentViewController = UIHostingController(rootView: rootView)
-
         guard FeatureProvider.isAvailable(.markets) else {
-            return contentViewController
+            return UIHostingController(rootView: rootView)
         }
 
-        let overlayViewController = UIHostingController(rootView: makeOverlayView())
+        let adapter = OverlayContentContainerViewControllerAdapter()
+        let rootView = rootView.environment(\.overlayContentContainer, adapter)
+        let contentViewController = UIHostingController(rootView: rootView)
 
         // TODO: Andrey Fedorov - Adjust all numeric values here for different devices and safe area
-        return OverlayContentContainerViewController(
+        let containerViewController = OverlayContentContainerViewController(
             contentViewController: contentViewController,
-            overlayViewController: overlayViewController,
             overlayCollapsedHeight: 92.0, // https://www.figma.com/design/91bpyCrISuWSvUzTLmcYRc/iOS-%E2%80%93-Draft?node-id=21140-91435&t=Z1kPdSQJ0JLoYgW0-4
             overlayExpandedVerticalOffset: 54.0 // https://www.figma.com/design/91bpyCrISuWSvUzTLmcYRc/iOS-%E2%80%93-Draft?node-id=22985-125042&t=Z1kPdSQJ0JLoYgW0-4
         )
-    }
 
-    private func makeOverlayView() -> some View {
-        return OverlayContentView(
-            color: .green,
-            hasScrollView: true
-        )
-    }
-}
+        adapter.set(containerViewController)
 
-@available(*, deprecated, message: "Test only, remove")
-struct OverlayContentView: View {
-    let color: Color
-    let hasScrollView: Bool
-
-    @State private var pageTitleToShow: String? = nil
-
-    var body: some View {
-        NavigationView {
-            color
-                .overlay {
-                    if hasScrollView {
-                        List(0 ..< 100) { index in
-                            Button("Row #\(index)") {
-                                pageTitleToShow = "Page #\(index)"
-                            }
-                        }
-                        .listStyle(.plain)
-                    } else {
-                        Button("Show second page") {
-                            pageTitleToShow = "Second page"
-                        }
-                    }
-                }
-                .navigationTitle("First page")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationLinks(links)
-        }
-        .navigationViewStyle(.stack)
-    }
-
-    @ViewBuilder
-    private var links: some View {
-        NavHolder()
-            .navigation(item: $pageTitleToShow) { pageTitleToShow in
-                color
-                    .overlay {
-                        VStack {
-                            Text(pageTitleToShow)
-                        }
-                    }
-                    .navigationTitle(pageTitleToShow)
-                    .navigationBarTitleDisplayMode(.inline)
-            }
+        return containerViewController
     }
 }
