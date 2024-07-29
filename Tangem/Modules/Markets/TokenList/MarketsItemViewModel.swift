36a4095bf24cae9d9280b30e85b63f5200417bad
Service Account <service@example.com>
2024-07-29 21:43:05 +0200

    IOS-7108: Re-use mapper for preview charts
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

--- Tangem/Modules/Markets/TokenList/MarketsItemViewModel.swift
+++ Tangem/Modules/Markets/TokenList/MarketsItemViewModel.swift
@@ -144,20 +144,17 @@ class MarketsItemViewModel: Identifiable, ObservableObject {
         from chartsDictionary: [String: [MarketsPriceIntervalType: MarketsChartsHistoryItemModel]],
         with interval: MarketsPriceIntervalType
     ) {
-        guard let chart = chartsDictionary.first(where: { $0.key == tokenId }) else {
+        guard
+            let chart = chartsDictionary.first(where: { $0.key == tokenId }),
+            let model = chart.value[interval]
+        else {
             return
         }
 
-        let chartsDoubleConvertedValues = makeChartsValue(from: chart.value[interval])
-        charts = chartsDoubleConvertedValues
-    }
-
-    private func makeChartsValue(from model: MarketsChartsHistoryItemModel?) -> [Double]? {
-        guard let model else { return nil }
-
-        let chartsDecimalValues: [Decimal] = model.prices.values.map { $0 }
-        let chartsDoubleConvertedValues: [Double] = chartsDecimalValues.map { NSDecimalNumber(decimal: $0).doubleValue }
-        return chartsDoubleConvertedValues
+        let mapper = TokenMarketsHistoryChartMapper()
+        charts = try? mapper
+            .mapAndSortValues(from: model)
+            .map(\.price.doubleValue)
     }
 }
 
