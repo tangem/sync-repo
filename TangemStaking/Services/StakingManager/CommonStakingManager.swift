b3d58f5e7c62da54782a6a08a71cd977e5cdf973
Service Account <service@example.com>
2024-08-06 10:46:55 +0200

    IOS-7562 Specify validator in the unstaking flow

--- TangemStaking/Services/StakingManager/CommonStakingManager.swift
+++ TangemStaking/Services/StakingManager/CommonStakingManager.swift
@@ -59,9 +59,14 @@ extension CommonStakingManager: StakingManager {
     func transaction(action: StakingActionType) async throws -> StakingTransactionInfo {
         switch (state, action) {
         case (.availableToStake(let yieldInfo), .stake(let amount, let validator)):
-            try await getTransactionToStake(amount: amount, validator: validator, integrationId: yieldInfo.id)
-        case (.staked(_, _), .unstake):
-            throw StakingManagerError.notImplemented // TODO: https://tangem.atlassian.net/browse/IOS-6898
+            return try await getTransactionToStake(amount: amount, validator: validator, integrationId: yieldInfo.id)
+
+        case (.staked(let balances, let yieldInfo), .unstake(let validator)):
+            guard let balance = balances.first(where: { $0.validatorAddress == validator }) else {
+                throw StakingManagerError.stakedBalanceNotFound(validator: validator)
+            }
+
+            return try await getTransactionToUnstake(amount: balance.blocked, validator: validator, integrationId: yieldInfo.id)
         default:
             throw StakingManagerError.stakingManagerStateNotSupportTransactionAction(action: action)
         }
@@ -104,6 +109,22 @@ private extension CommonStakingManager {
 
         return transaction
     }
+
+    func getTransactionToUnstake(amount: Decimal, validator: String, integrationId: String) async throws -> StakingTransactionInfo {
+        let action = try await provider.exitAction(
+            amount: amount, address: wallet.address,
+            validator: validator,
+            integrationId: integrationId
+        )
+
+        let transactionId = action.transactions[action.currentStepIndex].id
+        // We have to wait that stakek.it prepared the transaction
+        // Otherwise we may get the 404 error
+        try await Task.sleep(nanoseconds: 1 * NSEC_PER_SEC)
+        let transaction = try await provider.patchTransaction(id: transactionId)
+
+        return transaction
+    }
 }
 
 // MARK: - Log
@@ -116,6 +137,7 @@ private extension CommonStakingManager {
 
 public enum StakingManagerError: Error {
     case stakingManagerStateNotSupportTransactionAction(action: StakingActionType)
+    case stakedBalanceNotFound(validator: String)
     case notImplemented
     case notFound
 }
