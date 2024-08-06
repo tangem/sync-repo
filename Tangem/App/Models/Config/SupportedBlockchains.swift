449e7e1b9e7e622bf1a62cfb2df3e849bd7113af
Service Account <service@example.com>
2024-08-07 01:28:51 +0200

    IOS-7275 Add to supported blockchains

--- Tangem/App/Models/Config/SupportedBlockchains.swift
+++ Tangem/App/Models/Config/SupportedBlockchains.swift
@@ -60,6 +60,7 @@ struct SupportedBlockchains {
         [
             .mantle(testnet: false),
             .internetComputer(curve: .secp256k1),
+            .blast(testnet: false),
         ]
     }
 
