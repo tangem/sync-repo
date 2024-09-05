1fcea5da8d3d8fea5b9fa85d060274f63d2f3e8c
Service Account <service@example.com>
2024-09-05 13:17:37 +0300

    IOS-7804 Use `SensitiveText` in Staking

--- Tangem/Modules/StakingDetails/StakingDetailsViewModel.swift
+++ Tangem/Modules/StakingDetails/StakingDetailsViewModel.swift
@@ -161,7 +161,7 @@ private extension StakingDetailsViewModel {
             ),
             DefaultRowViewModel(
                 title: Localization.stakingDetailsAvailable,
-                detailsType: .text(walletModel.availableBalanceFormatted.crypto)
+                detailsType: .text(walletModel.availableBalanceFormatted.crypto, sensitive: true)
             ),
         ]
 
