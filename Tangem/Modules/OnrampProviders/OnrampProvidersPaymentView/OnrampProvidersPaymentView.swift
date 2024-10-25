a7e967f42f8fc3098bd61f52768c535b6000bceb
Service Account <service@example.com>
2024-10-25 17:51:13 +0100

    IOS-8270 Add `OnrampProvidersPaymentView `

---
+++ Tangem/Modules/OnrampProviders/OnrampProvidersPaymentView/OnrampProvidersPaymentView.swift
@@ -0,0 +1,15 @@
+//
+//  OnrampProvidersPaymentView.swift
+//  TangemApp
+//
+//  Created by Sergey Balashov on 25.10.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+
+struct OnrampProvidersPaymentView: View {
+    var body: some View {
+        Text("OnrampProvidersPaymentView")
+    }
+}
