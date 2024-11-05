7ffd1c03795b1ee2e3df416a1fc31abc3c8d9cac
Service Account <service@example.com>
2024-11-05 16:26:08 +0100

    IOS-8357 Add Currencies module

---
+++ Tangem/Modules/OnrampCurrencySelector/OnrampCurrencySelectorView.swift
@@ -0,0 +1,107 @@
+//
+//  OnrampCurrencySelectorView.swift
+//  TangemApp
+//
+//  Created by Aleksei Muraveinik on 3.11.24..
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import SwiftUI
+import TangemExpress
+
+struct OnrampCurrencySelectorView: View {
+    @ObservedObject var viewModel: OnrampCurrencySelectorViewModel
+
+    var body: some View {
+        VStack(alignment: .center, spacing: .zero) {
+            GrabberViewFactory().makeSwiftUIView()
+
+            CustomSearchBar(
+                searchText: $viewModel.searchText,
+                placeholder: Localization.onrampCurrencySearch,
+                keyboardType: .alphabet,
+                style: .translucent,
+                clearButtonAction: {
+                    viewModel.searchText = ""
+                }
+            )
+            .padding(.horizontal, 16)
+
+            switch viewModel.currencies {
+            case .loading:
+                skeletonsView
+            case .loaded(let currencies):
+                sectionsView(for: currencies)
+            case .failedToLoad:
+                failedToLoadView
+            }
+        }
+        .background(
+            Colors.Background.primary
+                .ignoresSafeArea()
+        )
+    }
+}
+
+private extension OnrampCurrencySelectorView {
+    @ViewBuilder
+    var failedToLoadView: some View {
+        Spacer()
+
+        MarketsUnableToLoadDataView(
+            isButtonBusy: false,
+            retryButtonAction: viewModel.loadCurrencies
+        )
+
+        Spacer()
+    }
+
+    var skeletonsView: some View {
+        ScrollView {
+            LazyVStack(alignment: .leading, spacing: .zero) {
+                sectionViewTitle(Localization.onrampCurrencyPopular)
+                ForEach(0 ..< 6) { _ in
+                    OnrampCurrencySkeletonView()
+                }
+
+                sectionViewTitle(Localization.onrampCurrencyOther)
+                ForEach(0 ..< 20) { _ in
+                    OnrampCurrencySkeletonView()
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+    }
+
+    func sectionsView(for state: OnrampCurrencySelectorState) -> some View {
+        ScrollView {
+            LazyVStack(alignment: .leading, spacing: .zero) {
+                switch state {
+                case .searched(let searched):
+                    ForEach(searched) { data in
+                        OnrampCurrencyView(data: data)
+                    }
+
+                case .sectioned(let popular, let other):
+                    sectionViewTitle(Localization.onrampCurrencyPopular)
+                    ForEach(popular) { data in
+                        OnrampCurrencyView(data: data)
+                    }
+
+                    sectionViewTitle(Localization.onrampCurrencyOther)
+                    ForEach(other) { data in
+                        OnrampCurrencyView(data: data)
+                    }
+                }
+            }
+            .padding(.horizontal, 16)
+        }
+    }
+
+    func sectionViewTitle(_ title: String) -> some View {
+        Text(title)
+            .style(Fonts.Bold.footnote, color: Colors.Text.tertiary)
+            .padding(.top, 12)
+            .padding(.bottom, 8)
+    }
+}
---
+++ Tangem/Modules/OnrampCurrencySelector/OnrampCurrencySelectorViewModel.swift
@@ -0,0 +1,115 @@
+//
+//  OnrampCurrencySelectorViewModel.swift
+//  TangemApp
+//
+//  Created by Aleksei Muraveinik on 3.11.24..
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Combine
+import TangemExpress
+
+enum OnrampCurrencySelectorState {
+    case sectioned(popular: [OnrampCurrencyViewData], other: [OnrampCurrencyViewData])
+    case searched([OnrampCurrencyViewData])
+}
+
+final class OnrampCurrencySelectorViewModel: Identifiable, ObservableObject {
+    @Published var searchText: String = ""
+    @Published private(set) var currencies: LoadingValue<OnrampCurrencySelectorState> = .loading
+
+    private let repository: OnrampRepository
+    private let dataRepository: OnrampDataRepository
+    private weak var coordinator: OnrampCurrencySelectorRoutable?
+
+    private let currenciesSubject = PassthroughSubject<[OnrampFiatCurrency], Never>()
+
+    init(
+        repository: OnrampRepository,
+        dataRepository: OnrampDataRepository,
+        coordinator: OnrampCurrencySelectorRoutable
+    ) {
+        self.repository = repository
+        self.dataRepository = dataRepository
+        self.coordinator = coordinator
+
+        Publishers.CombineLatest(
+            currenciesSubject,
+            $searchText
+        )
+        .withWeakCaptureOf(self)
+        .map { viewModel, data in
+            let (currencies, searchText) = data
+            return .loaded(
+                viewModel.mapToState(
+                    currencies: currencies,
+                    searchText: searchText
+                )
+            )
+        }
+        .receive(on: DispatchQueue.main)
+        .assign(to: &$currencies)
+
+        loadCurrencies()
+    }
+
+    func loadCurrencies() {
+        currencies = .loading
+        Task {
+            do {
+                let currencies = try await dataRepository.currencies()
+                currenciesSubject.send(currencies)
+            } catch {
+                currencies = .failedToLoad(error: error)
+            }
+        }
+    }
+}
+
+private extension OnrampCurrencySelectorViewModel {
+    func mapToState(
+        currencies: [OnrampFiatCurrency],
+        searchText: String
+    ) -> OnrampCurrencySelectorState {
+        if !searchText.isEmpty {
+            return .searched(
+                SearchUtil.search(
+                    currencies,
+                    in: \.identity.name,
+                    for: searchText
+                )
+                .map(mapToCurrencyViewData)
+            )
+        }
+
+        var popular = [OnrampCurrencyViewData]()
+        var other = [OnrampCurrencyViewData]()
+
+        let popularFiatCodes = dataRepository.popularFiatCodes
+        for currency in currencies {
+            if popularFiatCodes.contains(currency.identity.code) {
+                popular.append(mapToCurrencyViewData(currency: currency))
+            } else {
+                other.append(mapToCurrencyViewData(currency: currency))
+            }
+        }
+
+        return .sectioned(
+            popular: popular,
+            other: other
+        )
+    }
+
+    func mapToCurrencyViewData(currency: OnrampFiatCurrency) -> OnrampCurrencyViewData {
+        OnrampCurrencyViewData(
+            image: currency.identity.image,
+            code: currency.identity.code,
+            name: currency.identity.name,
+            isSelected: repository.preferenceCurrency == currency,
+            action: { [weak self] in
+                self?.repository.updatePreference(currency: currency)
+                self?.coordinator?.dismissCurrencySelector()
+            }
+        )
+    }
+}
