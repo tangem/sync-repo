1a0c098a448905597dbc20c5500a46319646a0e7
Service Account <service@example.com>
2024-11-11 23:03:56 +1100

    Add localization strings

--- Tangem/Modules/Main/ActionButtonsBuy/Models/BuyTokenSelectorStrings.swift
+++ Tangem/Modules/Main/ActionButtonsBuy/Models/BuyTokenSelectorStrings.swift
@@ -8,7 +8,7 @@
 
 struct BuyTokenSelectorStrings: TokenSelectorLocalizable {
     let availableTokensListTitle = Localization.exchangeTokensAvailableTokensHeader
-    let unavailableTokensListTitle = "Unavailable to purchase"
-    let emptySearchMessage = "Token not found in your portfolio? Check the Markets to find and add it for purchase"
+    let unavailableTokensListTitle = Localization.tokensListUnavailableToPurchaseHeader
+    let emptySearchMessage = Localization.actionButtonsBuyEmptySearchMessage
     let emptyTokensMessage: String? = nil
 }
