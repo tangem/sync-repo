35d5333b3dcbfacaf485225e5aa735ed78c1f022
Service Account <service@example.com>
2024-12-02 13:42:17 +0000

    IOS-8604 Update OnrampCountryDetectionCoordinatorView.swift

--- Tangem/Modules/OnrampCountryDetection/OnrampCountryDetectionCoordinatorView.swift
+++ Tangem/Modules/OnrampCountryDetection/OnrampCountryDetectionCoordinatorView.swift
@@ -14,7 +14,9 @@ struct OnrampCountryDetectionCoordinatorView: CoordinatorView {
     var body: some View {
         if let rootViewModel = coordinator.rootViewModel {
             OnrampCountryDetectionView(viewModel: rootViewModel)
-                // We have to use `overlay` instead of ZStack to save the view size
+                // We have to save the `OnrampCountryDetectionView` size because it's a bottom sheet
+                // If we will use `ZStack` the `CoordinatorView` will be expand on the whole screen
+                // The `overlay` doesn't impact on `CoordinatorView` size
                 .overlay(content: { sheets })
         }
     }
