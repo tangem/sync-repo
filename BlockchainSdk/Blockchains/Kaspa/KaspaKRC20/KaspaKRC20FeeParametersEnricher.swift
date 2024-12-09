6e589538e191defb87dafc969ee5fdc32168e0db
Service Account <service@example.com>
2024-12-09 13:05:00 +0100

    IOS-8684: Add helper for KRC20 fees
    Signed-off-by: Andrey Fedorov <afedorov@tangem.com>

---
+++ BlockchainSdk/Blockchains/Kaspa/KaspaKRC20/KaspaKRC20FeeParametersEnricher.swift
@@ -0,0 +1,41 @@
+//
+//  KaspaKRC20FeeParametersEnricher.swift
+//  BlockchainSdk
+//
+//  Created by m3g0byt3 on 09.12.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+import TangemSdk
+
+public struct KaspaKRC20FeeParametersEnricher {
+    private let existingFeeParameters: FeeParameters?
+
+    public init(existingFeeParameters: FeeParameters?) {
+        self.existingFeeParameters = existingFeeParameters
+    }
+
+    public func enrichCustomFeeIfNeeded(_ customFee: inout Fee) {
+        guard let parameters = existingFeeParameters as? KaspaKRC20.TokenTransactionFeeParams else {
+            return
+        }
+
+        var commitTransactionFee = parameters.commitFee
+        let revealTransactionFee = parameters.revealFee
+
+        assert(customFee.amount.type == commitTransactionFee.type, "Fee amount type inconsistency detected for commit tx")
+        assert(customFee.amount.type == revealTransactionFee.type, "Fee amount type inconsistency detected for reveal tx")
+
+        // The value of the reveal tx is fixed and has a constant value, so we calculate the new value of the commit tx fee
+        // as a remainder after subtracting the value of the reveal tx fee value from the total fee value
+        var newCommitTransactionFeeValue = customFee.amount.value - parameters.revealFee.value
+        if newCommitTransactionFeeValue < .zero {
+            newCommitTransactionFeeValue = .zero
+        }
+        commitTransactionFee.value = newCommitTransactionFeeValue
+
+        let newFeeParameters = KaspaKRC20.TokenTransactionFeeParams(commitFee: commitTransactionFee, revealFee: revealTransactionFee)
+        customFee = Fee(customFee.amount, parameters: newFeeParameters)
+    }
+}
