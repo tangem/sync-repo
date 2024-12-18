2d13254023773005c0b5bad29e51100e32a5eb1c
Service Account <service@example.com>
2024-12-18 10:32:44 +0000

    IOS-8838 Reorganize code

--- Tangem/Modules/TokenSectionsAdapter/TokenSectionsSourcePublisherFactory.swift
+++ Tangem/Modules/TokenSectionsAdapter/TokenSectionsSourcePublisherFactory.swift
@@ -14,13 +14,6 @@ import CombineExt
 /// method as the source of truth for creating token sections.
 struct TokenSectionsSourcePublisherFactory {
     func makeSourcePublisherForMainScreen(for userWalletModel: UserWalletModel) -> some Publisher<[WalletModel], Never> {
-        // The contents of the coins and tokens collection for the user wallet
-        let walletModelsPublisher = userWalletModel
-            .walletModelsManager
-            .walletModelsPublisher
-            .share(replay: 1)
-            .eraseToAnyPublisher()
-
         // Fiat balance changes for the coins and tokens for the user wallet
         let walletModelsBalanceChangesPublisher = userWalletModel
             .totalBalancePublisher
@@ -29,15 +22,19 @@ struct TokenSectionsSourcePublisherFactory {
             .eraseToAnyPublisher()
 
         return [
-            walletModelsPublisher,
+            walletModelsPublisher(for: userWalletModel),
             walletModelsBalanceChangesPublisher,
         ].merge()
     }
 
     /// Fix https://tangem.atlassian.net/browse/IOS-8671
     func makeSourcePublisher(for userWalletModel: UserWalletModel) -> some Publisher<[WalletModel], Never> {
-        // The contents of the coins and tokens collection for the user wallet
-        return userWalletModel
+        return walletModelsPublisher(for: userWalletModel)
+    }
+
+    // The contents of the coins and tokens collection for the user wallet
+    private func walletModelsPublisher(for userWalletModel: UserWalletModel) -> some Publisher<[WalletModel], Never> {
+        userWalletModel
             .walletModelsManager
             .walletModelsPublisher
             .share(replay: 1)
