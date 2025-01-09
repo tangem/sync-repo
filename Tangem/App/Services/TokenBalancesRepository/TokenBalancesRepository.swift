8ad46343846ad499f94f2fb03144899fcde1ea47
Service Account <service@example.com>
2025-01-09 15:18:29 +0000

    IOS-8906 Add `TokenBalancesRepository`

---
+++ Tangem/App/Services/TokenBalancesRepository/TokenBalancesRepository.swift
@@ -0,0 +1,36 @@
+//
+//  TokenBalancesRepository.swift
+//  TangemApp
+//
+//  Created by Sergey Balashov on 24.12.2024.
+//  Copyright © 2024 Tangem AG. All rights reserved.
+//
+
+import Foundation
+
+protocol TokenBalancesRepository {
+    func balance(wallet: WalletModel, type: CachedBalanceType) -> CachedBalance?
+
+    func store(balance: CachedBalance, for wallet: WalletModel, type: CachedBalanceType)
+}
+
+struct CachedBalance: Hashable, Codable {
+    let balance: Decimal
+    let date: Date
+}
+
+enum CachedBalanceType: String, Hashable, Codable {
+    case available
+    case staked
+}
+
+private struct TokenBalancesRepositoryKey: InjectionKey {
+    static var currentValue: TokenBalancesRepository = CommonTokenBalancesRepository()
+}
+
+extension InjectedValues {
+    var tokenBalancesRepository: TokenBalancesRepository {
+        get { Self[TokenBalancesRepositoryKey.self] }
+        set { Self[TokenBalancesRepositoryKey.self] = newValue }
+    }
+}
